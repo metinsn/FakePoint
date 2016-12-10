0faa87a3afcc9749492c9049472122acbfe89b4e
metin.sen <metin.sen@ALS-YG39506-J06>
2016-12-10 16:47:34 +0300

    1

---
+++ FakePoint/FakePoint/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FakePoint
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Graphics g;
+        ColorDialog colorDialog = new ColorDialog();
+        int kalinlik = 3;
+        int baslaX, baslaY;
+        bool ciz;
+
+        private void renksecBtn_Click(object sender, EventArgs e)
+        {
+            colorDialog.ShowDialog();
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            ciz = true;
+            baslaX = e.X;
+            baslaY = e.Y;
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            ciz = false;
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            g = CreateGraphics();
+            Pen P = new Pen(colorDialog.Color, kalinlik);
+            Point Point1 = new Point(baslaX, baslaY);
+            Point Point2 = new Point(e.X, e.Y);
+
+            if (ciz == true)
+            {
+                g.DrawLine(P, Point1, Point2);
+                baslaX = e.X;
+                baslaY = e.Y;
+            }
+        }
+
+
+    }
+}
